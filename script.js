d47f9c9bcae1226fd0b1960024ebbbeca82df51d
unnikrishnansk <unni.sk94@gmail.com>
2023-09-03 11:31:46 +0530

    commit db connection

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/buyzoneregister")
+mongoose.connect("mongodb+srv://unnisk:Anjali*8075#@buyzoneclust.6iab9lo.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log("mongodb connected")
 })
